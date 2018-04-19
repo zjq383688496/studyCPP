42beec6b08d240acfa696f09bbca7e7d2439bc14
jimmy.zhuang <zhuangjiaqi@rongyi.com>
2018-04-19 20:38:44 +0800

    update

--- lesson/00_hello/main.cpp
+++ lesson/00_hello/main.cpp
@@ -2,5 +2,8 @@
 
 int main() {
     std::cout << "hello Jimmy!" << std::endl;
+    int v1 = 0, v2 = 0, v3 = 0;
+    std::cin >> v1 >> v2 >> v3;
+    std::cout << v1 << " 加 " << v2 << " 乘以 " << v3 << " 等于 " << (v1 + v2) * v3 << std::endl;
     return 0;
 }
